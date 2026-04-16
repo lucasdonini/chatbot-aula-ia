e9b4a2193b273ea4c7ecf9c38fa4a0bf4ee54380
Lucas Kluska Donini <lkd.lucas@gmail.com>
2026-04-16 10:30:13 -0300

    feat(agents): add functions and configurations to chain router -> specialist -> orquestrator

--- src/agents/__init__.py
+++ src/agents/__init__.py
@@ -2,3 +2,5 @@ from .router import router_app
 from .financial import financial_app
 from .agenda import agenda_app
 from .orquestrator import orquestrator_app
+
+SPECIALISTS = {"financeiro": financial_app, "agenda": agenda_app}
--- src/main.py
+++ src/main.py
@@ -1,6 +1,33 @@
-from .agents import router_app, financial_app, agenda_app, orquestrator_app
-from datetime import datetime
-import os
+from .agents import router_app, orquestrator_app, SPECIALISTS
+import os, re
+
+
+def invoke_agent(agent, user_input: str, session_id: str) -> str:
+    response = agent.invoke(
+        {"messages": [{"role": "human", "content": user_input}]},
+        config={"configurable": {"thread_id": session_id}},
+    )
+    return response["messages"][-1].text
+
+
+def make_question(user_input: str, session_id: str) -> str:
+    try:
+        router_response = invoke_agent(router_app, user_input, session_id)
+
+        match = re.search(r"(?<=ROUTE=)\w+", router_response)
+        if not match:
+            return router_response
+
+        specialist_name = match.group()
+        if not specialist_name in SPECIALISTS:
+            return f"Erro no roteador: agente {specialist_name} não encontrado"
+
+        specialist = SPECIALISTS[specialist_name]
+        specialist_response = invoke_agent(specialist, router_response, session_id)
+        return invoke_agent(orquestrator_app, specialist_response, session_id)
+
+    except Exception as e:
+        print(f"Erro no roteador: {e}")
 
 
 os.system("cls")
@@ -11,16 +38,6 @@ while True:
     if user_input.lower() in ("sair", "exit", "tchau", "bye", "end", "fim"):
         print("Encerrando a conversa")
         break
-    try:
-        response = router_app.invoke(
-            {
-                "messages": [
-                    {"role": "human", "content": f"{datetime.now()} -> {user_input}"}
-                ]
-            },
-            config={"configurable": {"thread_id": "meu_id_de_sessao"}},
-        )
-        print(f"\n{response['messages'][-1].text}\n{'-' * 20}\n")
-    except Exception as e:
-        print("Erro ao consumir API:", e)
-        break
+
+    response = make_question(user_input, "meu_id_de_sessao")
+    print(f"\n{response}\n{'-' * 20}\n")
