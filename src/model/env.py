0e31e20fa1e3e0f234518f7bc9b350d85e44fa99
Lucas Kluska Donini <lkd.lucas@gmail.com>
2026-04-30 11:59:49 -0300

    fix(env): add ignore extra in pyenv_setting env mapping

--- src/model/env.py
+++ src/model/env.py
@@ -1,4 +1,4 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class EnvVariables(BaseSettings):
@@ -6,7 +6,7 @@ class EnvVariables(BaseSettings):
     groq_api_key: str
     database_url: str
 
-    model_config = {"env_file": ".env"}
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
 
 env = EnvVariables()
