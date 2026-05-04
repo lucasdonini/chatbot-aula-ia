d669fbee4eeb97c56aa40c1cf807b9670043cc29
Lucas Kluska Donini <lkd.lucas@gmail.com>
2026-05-04 13:00:01 -0300

    refactor(clean code): separate make_question into multiple functions

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ from .agents import router_app, orquestrator_app, SPECIALISTS, CONSULTANTS
 from .infrastructure.md_console import print
 from .infrastructure.logger import setup_logger
 
+from typing import Optional
+
 import logging
 import os
 import re
@@ -18,44 +20,58 @@ def invoke_agent(agent, user_input: str, session_id: str) -> str:
     return response["messages"][-1].text
 
 
+def invoke_specialist(specialist, router_response: str, session_id: str) -> str:
+    specialist_response = invoke_agent(specialist, router_response, session_id)
+    logger.debug("Specialist response: %s", specialist_response)
+
+    logger.info("Agent invoked: orquestrator -> input = %s", specialist_response)
+    response = invoke_agent(orquestrator_app, specialist_response, session_id)
+    logger.debug("Orquestrator response: %s", response)
+    return response
+
+
+def invoke_consultant(consultant, router_response: str, session_id: str) -> str:
+    response = invoke_agent(consultant, router_response, session_id)
+    logger.debug("Consultant response: %s", response)
+    return response
+
+
+def match_agent(router_response: str) -> Optional[str]:
+    match = re.search(r"(?<=ROUTE=)\w+", router_response)
+    if not match:
+        return None
+
+    agent_name = match.group()
+    logger.debug("Matched agent: %s from input: %s", agent_name, router_response)
+    return agent_name
+
+
 def make_question(user_input: str, session_id: str) -> str:
     try:
-        logger.info("Agent invoked: router")
+        logger.info("Agent invoked: router -> input = %s", user_input)
         router_response = invoke_agent(router_app, user_input, session_id)
         logger.debug("Router response: %s", router_response)
 
-        match = re.search(r"(?<=ROUTE=)\w+", router_response)
-        if not match:
+        if not (agent_name := match_agent(router_response)):
+            logger.info("No agent matched. Router answering directly to user.")
             return router_response
-        agent_name = match.group()
-        logger.debug("Matched agent: %s from input: %s", agent_name, router_response)
 
         if specialist := SPECIALISTS.get(agent_name):
             logger.info(
                 "Specialist invoked: %s -> input = %s", agent_name, router_response
             )
-            specialist_response = invoke_agent(specialist, router_response, session_id)
-            logger.debug("Specialist response: %s", specialist_response)
-
-            logger.info(
-                "Agent invoked: orquestrator -> input = %s", specialist_response
-            )
-            response = invoke_agent(orquestrator_app, specialist_response, session_id)
-            logger.debug("Orquestrator response: %s", response)
-            return response
+            return invoke_specialist(specialist, router_response, session_id)
 
         if consultant := CONSULTANTS.get(agent_name):
             logger.info(
                 "Consultant invoked: %s -> input = %s", agent_name, router_response
             )
-            response = invoke_agent(consultant, router_response, session_id)
-            logger.debug("Consultant response: %s", response)
-            return response
+            return invoke_consultant(consultant, router_response, session_id)
 
         logger.error("Router agent tried to access an unexisting agent: %s", agent_name)
         return f"Erro no roteador: agente {agent_name} não encontrado"
 
-    except Exception as e:
+    except Exception:
         logger.exception("Exception raised while invoking agent")
         return f"Sinto muito, tivemos um erro interno. Tente novamente mais tarde."
 
