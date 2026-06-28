518bb6a664610581d9adc3da862920aa70551b8d
lucasdonini <lkd.lucas@gmail.com>
2026-06-28 17:15:07 -0300

    fix(CI): add default values for env vars in settings.py

--- src/infrastructure/settings.py
+++ src/infrastructure/settings.py
@@ -2,10 +2,10 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    gemini_api_key: str
-    groq_api_key: str
+    gemini_api_key: str = ""
+    groq_api_key: str = ""
 
-    postgres_url: str
+    postgres_url: str = ""
 
     mongodb_uri: str = "mongodb://localhost:27017"
     mongodb_dbname: str = "assessoria"
